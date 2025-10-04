e8be63893de4a4301b8949c49eea8e7e18123660
Washington <lepexgamer102000@gmail.com>
2025-10-04 17:21:15 -0300

    feat: aprendizado, indexação de lista

---
+++ Cap 1 - Exploração e Limpeza de Dados/1_endexacao_de_lista.py
@@ -0,0 +1,12 @@
+example_list = list(range(1,6))
+print(example_list)
+
+print(example_list[0])
+
+print(example_list[-1])
+
+print(example_list[:3])
+
+example_list[0] = 'Stringzinha'
+
+print(example_list[0])
